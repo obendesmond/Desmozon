a5c01afeb221e83ca81fd9ed6da3038567265269
obendesmond <obendesmond2@gmail.com>
2022-03-11 22:22:47 +0100

    implement banner section (hero slider)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
+import Banner from "components/Banner";
 import Header from "components/Header";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Desmozon | Amazon 2.0</title>
       </Head>
 
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+
+        {/* product feed */}
+      </main>
     </div>
   );
 }
